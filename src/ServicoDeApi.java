03338156cea29566149ec0f161f0e00eb9ba2930
Gustavolrsc <gustavolrsc@gmail.com>
2024-10-11 12:05:23 -0300

    Initial commit

---
+++ src/ServicoDeApi.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+
+public class ApiService {
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/b4894db8127b127f00d015da/latest/USD";
+    private HttpClient client;
+
+    public ApiService() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public CurrencyData getExchangeRates() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+
+        Gson gson = new Gson();
+        return gson.fromJson(responseBody, CurrencyData.class);
+    }
+}
